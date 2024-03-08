0ad142b3b7c61427ea53f2e42b8ec605d5ab84c3
Christian Velez <christian.gerardo_@outlook.es>
2024-03-07 21:11:14 -0600

    change metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-   title: 'FolderArt by christianvm',
-   description: 'Generate folder icons for macOS',
+   title: 'Folder Icon Maker - FolderArt by christianvm',
+   description: 'Create custom icons for files or folders for macOS',
+   metadataBase: new URL('https://folderart.christianvm.dev'),
 }
 
 export default function RootLayout({
