6ef93a62ce71e9e9c51e150809f00d83035af6f3
Christian Velez <christian.gerardo_@outlook.es>
2024-03-12 17:17:29 -0600

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import { Analytics } from '@vercel/analytics/react'
 import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
 import './globals.css'
@@ -16,7 +17,10 @@ export default function RootLayout({
 }>) {
    return (
       <html lang='en' className={`${GeistSans.variable} ${GeistMono.variable}`}>
-         <body>{children}</body>
+         <body>
+            {children}
+            <Analytics />
+         </body>
       </html>
    )
 }
